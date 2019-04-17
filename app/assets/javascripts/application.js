8c55f80e4cdccc0ce19eb00024eb6077599ef0e4
Ben Emrick <benemrick@gmail.com>
2019-04-16 23:21:18 -0400

    Asset pipeline maintenance

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require_tree .
 //= require activestorage
 //= require turbolinks
-//= require_tree .
-//= require jquery3
-//= require bootstrap-sprockets
-//= require popper
 //= require tether
