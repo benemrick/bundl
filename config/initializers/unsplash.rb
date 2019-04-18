7ca6542496db5aaea9ae5618d5626ca4c28fca61
Ben Emrick <benemrick@gmail.com>
2019-04-17 22:32:38 -0400

    Unsplash API setup

---
+++ config/initializers/unsplash.rb
@@ -0,0 +1,6 @@
+Unsplash.configure do |config|
+  config.application_access_key = "81851672423111eaa3f3cf61f3a4612c4c293a7370c99414b8beb86bdda997c3"
+  config.application_secret = "962557853c3008ed783f609372424298310d91623cb25656ddabe08c296a4db8"
+  config.application_redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
+  config.utm_source = "bundl_app"
+end
