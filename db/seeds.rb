a117b2676d49056a5cd4e832195c86da1d4a1c31
Ben Emrick <benemrick@gmail.com>
2019-04-22 17:19:00 -0400

    Add seed to populate db with initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+bundls = Bundl.create([{ title: "Ruby on Rails", description: "Web development in Ruby on Rails" }, { title: "Technology", description: "My collection of news related to technology and electronics" }])
+bundl = bundls.first
+bundl.media_items.create(name: "Getting Started Ruby on Rails", description: "Ruby on Rails official guide for beginning up an app", url: "https://guides.rubyonrails.org/getting_started.html", source: "Ruby on Rails", bundl_id: bundl)
